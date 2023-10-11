b8ff5f57ee07017fd1fff50d2001cf26558b5e5a
Ian Kim <ian@ianmkim.com>
2023-10-10 22:12:16 -0400

    fixed dockerfile and index

--- backend/src/routers/index.py
+++ backend/src/routers/index.py
@@ -7,4 +7,5 @@ router = APIRouter()
 @router.get("/")
 async def index() -> Success:
     return Success(ok=True,
-                   message = "Hello world")
+                   message = "Hello world",
+                   error = None)
