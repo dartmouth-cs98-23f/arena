c5341ca1b4d8bf6c7f75b517131eee5d54e4e7a0
Ian Kim <42292798+ianmkim@users.noreply.github.com>
2023-10-18 16:07:59 -0400

    added API authentication (#39)
    * added API authentication
    
    * added greenlet requirement

---
+++ backend/src/auth.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from fastapi.security.api_key import APIKeyHeader
+from fastapi import Security, HTTPException, Depends
+from starlette.status import HTTP_403_FORBIDDEN
+
+from sqlalchemy.orm import Session
+from backend.src.models.database import SessionLocal, engine, get_db
+from backend.src.model.keys import Key, KeyUse
+
+from datetime import datetime
+
+api_key_header = APIKeyHeader(name="access_token", auto_error=False)
+
+async def get_api_key(api_key_header:str = Security(api_key_header),
+                      db:Session = Depends(get_db)) -> Optional[str]:
+    keyobj = db.query(Key).filter(Key.key == api_key_header).first()
+    if keyobj is not None:
+        useRecord = KeyUse(date = datetime.now())
+        keyobj.utilization.append(useRecord)
+        db.add(keyobj)
+        db.commit()
+        return api_key_header
+
+    raise HTTPException(
+        status_code = HTTP_403_FORBIDDEN,
+        detail = "Could not validate API Key")
