6a06305cbff3daaee7430f70461190259182e2e0
Jeffrey Lam <jeffrey.q.lam@gmail.com>
2023-10-23 16:59:07 -0400

    add adjust_odds

---
+++ experiments/adjust_odds.py
@@ -0,0 +1,26 @@
+from random import random
+
+def modify_odds(last_yes, last_no, prob_history):
+  if last_yes == None and last_no == None: # no bets
+    if len(prob_history) < 2:
+      return prob_history[-1] - (random() * 5)
+    else:
+      if prob_history[-2] < prob_history[-1]:
+        new = prob_history[-2] - (random() * 13) + (random() * 3)
+        if new < 5:
+          return 5 + random() * 2
+        else:
+          return new
+      else:
+        new = prob_history[-2] + (random() * 13) - (random() * 3)
+        if new > 95:
+          return 95 - random() * 2
+        else:
+          return new
+  else:
+    if last_yes == None:
+      return (last_no / 2) + (random() * 5)
+    elif last_no == None:
+      return ((last_yes + 100) / 2) - (random() * 5)
+    else:
+      return ((last_yes + last_no) / 2) - (random() * 5) + (random() * 5)
