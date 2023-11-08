8aab4e55469f045ef8263081a7312e20151f61e2
Ravin Anderson <ravinanderson@Ravins-MacBook-Pro.local>
2023-11-08 01:44:57 -0500

    added oauth functions

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import Depends, FastAPI
-
+from starlette.middleware.sessions import SessionMiddleware
+import os
 from backend.src.routers import index
 from backend.src.routers import bets
 from backend.src.routers import users
@@ -29,6 +30,12 @@ app.include_router(bets.router,
 app.include_router(users.router,
                    prefix="/user",
                    tags=["user"])
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("SECRET_KEY"),
+    session_cookie="session",
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=5000, log_level="info")
--- backend/src/routers/index.py
+++ backend/src/routers/index.py
@@ -1,11 +1,48 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from fastapi.responses import HTMLResponse
+from starlette.responses import RedirectResponse
+from starlette.config import Config
+from starlette.requests import Request
+from authlib.integrations.starlette_client import OAuth
 from backend.src.schemas.index import Success
 
 router = APIRouter()
 
+oauth = OAuth()
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+    name='google',
+    client_id="988417806604-ggnkhrhere0el8b4r3ehko3ncmt8181r.apps.googleusercontent.com",
+    client_secret="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",
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': "openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
+    }
+)
+
 @router.get("/")
-async def index() -> Success:
-    return Success(ok=True,
-                   message = "Hello world",
-                   error = None)
+async def index(request: Request):
+    user = request.session.get('user')
+    if user:
+        name = user.get('name')
+        return HTMLResponse(f'<p>Hello {name}!</p><a href=/logout>Logout</a>')
+    return HTMLResponse('<a href=/login>Login</a>')
+
+@router.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+@router.get("/auth")
+async def auth(request: Request):
+    token = await oauth.google.authorize_access_token(request)
+    request.session['user'] = token['userinfo']
+    # You can now create a user session or a JWT token
+    # and redirect the user to the internal page with the token
+    return RedirectResponse(url='/')
+
+@router.get("/logout")
+async def logout(request: Request):
+    request.session.pop('user', None)
+    return RedirectResponse(url='/')
