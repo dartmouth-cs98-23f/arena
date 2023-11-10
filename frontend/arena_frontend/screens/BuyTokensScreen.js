0f58395f131185a0fd2cc16942135c50fdeacdd2
Ravin Anderson <ravinanderson@Ravins-MacBook-Pro.local>
2023-11-10 00:52:14 -0500

    update on buy page too

--- frontend/arena_frontend/screens/BuyTokensScreen.js
+++ frontend/arena_frontend/screens/BuyTokensScreen.js
@@ -40,7 +40,7 @@ function BuyTokensScreen({ route, navigation }) {
                     <Text style={styles.backIcon}>🔙</Text>
                 </TouchableOpacity>
                 <Text style={styles.headerTitle}>Buy Tokens</Text>
-                <Text style={styles.coinBalance}>50</Text>
+                <Text style={styles.coinBalance}>{myTokens}</Text>
             </View>
             <FlatList
                 data={tokenPackages}
