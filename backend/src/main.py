4c5c4e661d7d74d37e3aea6bac7fd2df433db1e1
Ian Kim <ian@ianmkim.com>
2024-01-10 12:27:26 -0500

    changed host to listen

--- backend/src/main.py
+++ backend/src/main.py
@@ -49,4 +49,4 @@ app.include_router(users.router,
                    tags=["user"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
