e22f05c699b1d58d129a9e3f9996f6753e6f34a7
Ian Kim <ian@ianmkim.com>
2024-01-31 20:29:24 -0500

    rollback-fix

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -13,36 +13,46 @@ from datetime import datetime
 
 api_key_header = APIKeyHeader(name="access_token", auto_error=False)
 
-async def get_api_key_from_state(request:Request) -> Optional[str]:
+
+async def get_api_key_from_state(request: Request) -> Optional[str]:
     if "access_token" not in request.headers:
         raise HTTPException(
-            status_code = HTTP_403_FORBIDDEN,
-            detail = "Could not find \"access_token\" in header",
+            status_code=HTTP_403_FORBIDDEN,
+            detail='Could not find "access_token" in header',
         )
 
     api_key_header = request.headers["access_token"]
     # we get database from the app state which is in the request
     print("API_KEY:", api_key_header)
-    keyobj = request.app.state.db.query(Key).filter(Key.key == api_key_header).first()
-    if(keyobj is not None):
+    try:
+        keyobj = (
+            request.app.state.db.query(Key).filter(Key.key == api_key_header).first()
+        )
+    except Exception as ex:
+        print(f"Rolling back because of invalid transaction: {ex}")
+        request.app.state.db.rollback()
+
+    if keyobj is not None:
         return str(keyobj.user_id)
 
     raise HTTPException(
-        status_code = HTTP_403_FORBIDDEN,
-        detail = "Could not validate API Key")
+        status_code=HTTP_403_FORBIDDEN, detail="Could not validate API Key"
+    )
 
-async def get_api_key(api_key_header:str = Security(api_key_header),
-                      db:Session = Depends(get_db)) -> Optional[str]:
+
+async def get_api_key(
+    api_key_header: str = Security(api_key_header), db: Session = Depends(get_db)
+) -> Optional[str]:
     keyobj = db.query(Key).filter(Key.key == api_key_header).first()
 
     if keyobj is not None:
         # delete API use records for performance
-        #useRecord = KeyUse(date = datetime.now())
-        #keyobj.utilization.append(useRecord)
-        #db.add(keyobj)
-        #db.commit()
+        # useRecord = KeyUse(date = datetime.now())
+        # keyobj.utilization.append(useRecord)
+        # db.add(keyobj)
+        # db.commit()
         return api_key_header
 
     raise HTTPException(
-        status_code = HTTP_403_FORBIDDEN,
-        detail = "Could not validate API Key")
+        status_code=HTTP_403_FORBIDDEN, detail="Could not validate API Key"
+    )
